77cbcb933afc98ca0e0b9de19c89deeb18e632c7
IJssel <54723855+IJIJI@users.noreply.github.com>
2021-08-13 12:21:45 +0200

    Funtional but blocking..

--- PTZ-v2.ino
+++ PTZ-v2.ino
@@ -1,59 +1,61 @@
 #include "Arduino.h"
 #include "TeensyStep.h"
 
+
+
+
 #define fan1Pin PA8
 
+
 // Define pin connections & motor's steps per revolution
-#define enPin PE3
-#define stepPin PE5
-#define dirPin PE6
-//#define enPin PC11
-//#define stepPin PA0
-//#define dirPin PC15
-#define stepsPerRevolution 200*6*4
+#define enXPin PC11
+#define enYPin PE3
+
+Stepper motorX(PA0, PC15);       // STEP pin: PE5, DIR pin: PE6
+// Stepper motorX(PA0, PC15), motorY(PE5, PE6);       // STEP pin: PE5, DIR pin: PE6
+StepControl stepController;
+// RotateControl rotateController;
+
+// TODO swap motor 1 and 2 pins
+
+#define stepsPerRevolutionX 200*6*4
 
 void setup()
 {
+
+  motorX
+    .setAcceleration(1000)
+    .setMaxSpeed(15000);
+
+
   // Declare pins as Outputs
-  pinMode(stepPin, OUTPUT);
-  pinMode(dirPin, OUTPUT);
-  pinMode(enPin, OUTPUT);
+  pinMode(enXPin, OUTPUT);
+  pinMode(enYPin, OUTPUT);
 
   pinMode(fan1Pin, OUTPUT);
 
   delay(500);
 
-  digitalWrite(enPin, LOW);
-  analogWrite(fan1Pin, 255);
+  digitalWrite(enXPin, LOW);
+  digitalWrite(enYPin, LOW);
+  digitalWrite(fan1Pin, HIGH);
+  // analogWrite(fan1Pin, 255); //TODO fix analogwrite
 
 
   delay(1000);
 }
 void loop()
 {
-  // Set motor direction clockwise
-  digitalWrite(dirPin, HIGH);
 
-  // Spin motor slowly
-  for(int x = 0; x < stepsPerRevolution; x++)
-  {
-    digitalWrite(stepPin, HIGH);
-    delayMicroseconds(2000);
-    digitalWrite(stepPin, LOW);
-    delayMicroseconds(2000);
-  }
+  motorX.setTargetAbs(1000);  // Set target position to 1000 steps from current position
+
+
+  stepController.move(motorX);    // Do the move
+
   delay(1000); // Wait a second
   
-  // Set motor direction counterclockwise
-  digitalWrite(dirPin, LOW);
+  motorX.setTargetAbs(-1000);  // Set target position to 1000 steps from current position
+  stepController.move(motorX);    // Do the move
 
-  // Spin motor quickly
-  for(int x = 0; x < stepsPerRevolution; x++)
-  {
-    digitalWrite(stepPin, HIGH);
-    delayMicroseconds(1000);
-    digitalWrite(stepPin, LOW);
-    delayMicroseconds(1000);
-  }
   delay(1000); // Wait a second
 }
